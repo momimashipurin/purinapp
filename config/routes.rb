6dddce0d6ec460d0d423fcfc8ff5aa1e909feea2
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-08-16 06:50:11 +0000

    [Add]public系全コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,42 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/clear'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/comfirm'
+    get 'orders/complete'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/check'
+    get 'customers/out'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
