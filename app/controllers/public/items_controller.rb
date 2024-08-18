0a5299074d49c4bd53d844adcd4e8bc8c9ebbdc8
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-08-18 16:01:46 +0000

    商品（一覧・詳細）画面作成完了

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,14 @@
 class Public::ItemsController < ApplicationController
+  
   def index
+    @items = Item.page(params[:page]).per(8)
+    @items_all = Item.all
+    # @genres = Genre.all　←表示必要であればジャンル一覧作成後追加by（みっころ）
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+  
 end
