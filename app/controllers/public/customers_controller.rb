65955334099a1b358119459c856605bd2a412ae7
gugugngn <173164252+gugugngn@users.noreply.github.com>
2024-08-19 14:18:51 +0000

    会員退会機能作成途中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,7 +10,12 @@ class Public::CustomersController < ApplicationController
 
   def check
   end
-
+  
   def out
+    @customer = current_customer
+    @customer.update(is_active: false)  #is_activeカラムをfalseにupdateして、退会状態にする。実際にはデータベース上の会員のレコードは削除されない。
+    reset_session      #セッション情報（個人の情報やアクションの履歴などの情報）をリセット
+    flash[:notice] = "退会しました"
+    redirect_to root_path
   end
 end
