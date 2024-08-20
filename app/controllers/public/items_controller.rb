8a09a18257ab6c7c6a0ced44a2cea9777535f26d
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-08-20 05:48:51 +0000

    【修正・追加】　商品（一覧・詳細）画面にジャンル一覧追加、商品一覧の画像にリンク付与、カート内一覧画面のカート空にする機能完成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,12 +3,13 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(1)
     @items_all = Item.all
-    # @genres = Genre.all　←表示必要であればジャンル一覧作成後追加by（みっころ）
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
   
 end
