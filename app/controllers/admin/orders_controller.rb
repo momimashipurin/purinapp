ffbe5f2570cfb36d69724b5ac7b9f7c94d5b57d6
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-08-20 06:22:31 +0000

    注文詳細ページの土台完了で一時保存（機能は未実装）

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,8 @@
 class Admin::OrdersController < ApplicationController
   def show
+    #@order = Order.find(params[:id])
+    @items = Item.all #注文した商品を全て表示したいからall?
+    @customer = Customer.find(params[:id])
   end
 
   def update
