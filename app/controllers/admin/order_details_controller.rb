f57e501e824bcf466166ae2d3a25851f8d24cf53
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-08-22 05:27:06 +0000

    注文詳細ページ作成中（ステータス紐づけ未実装）

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,17 +2,13 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_detail = OrderDetail.find(params[:id])
-    if @order_detail.update(order_params)
-      redirect_to admin_order_path(@order_detail.order.id), notice: '更新が成功しました' #更新できない
-      #redirect_to admin_order_path(@order_detail) #別の人のとこに遷移
-      #redirect_to admin_order_path#別の人のとこに遷移
-      #redirect_to admin_order_path(@order)
-    else
-      render "admin/"
-    end
+    @order_detail.update(order_params)
+    redirect_to admin_order_path(@order_detail.order), notice: '更新が成功しました'
+    
+    #注文ステータスが～～に変更されたら、制作ステータスを～～に変更
   end
 
-    private
+  private
 
   def order_params
     params.require(:order_detail).permit(:making_status)
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,7 +8,20 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-    render :show
+    redirect_to admin_order_path(@order)
+    # render :show
+
+    #制作ステータスが～～に変更されたら、注文ステータスを～～に変更
+    #商品ごとのステータスでさらに制限
+
+
+
+    # @order = Order.find(params[:id])
+    # @order_details = @order.order_details
+    # if @order.update(order_params)
+    #   @order_details.update_all(making_status: "制作待ち") if @order.status == "入金確認"
+    # end
+    # redirect_to admin_order_path(@order)
   end
 
   private
