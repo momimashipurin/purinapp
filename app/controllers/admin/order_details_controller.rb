0b0aa8242e91fff5626bcd068fdbfdd723601b2c
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-08-22 01:58:49 +0000

    注文詳細ページステータス機能以外実装完了（一時保存）

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,21 @@
 class Admin::OrderDetailsController < ApplicationController
+
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_params)
+      redirect_to admin_order_path(@order_detail.order.id), notice: '更新が成功しました' #更新できない
+      #redirect_to admin_order_path(@order_detail) #別の人のとこに遷移
+      #redirect_to admin_order_path#別の人のとこに遷移
+      #redirect_to admin_order_path(@order)
+    else
+      render "admin/"
+    end
   end
+
+    private
+
+  def order_params
+    params.require(:order_detail).permit(:making_status)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,22 +1,20 @@
 class Admin::OrdersController < ApplicationController
+
   def show
     @order = Order.find(params[:id])
-    
-    @intax = 0 #税込み価格を格納する変数
-    @sum = 0 #小計を格納する変数
-    @total_price = 0 #商品合計を格納する変数
-    
-    @order.order_details.each do |order_detail|
-      @intax = order_detail.item.price * 1.08 #税抜価格*1.08
-      @sum = @intax * @order.order_detail.amount #税込価格*数量
-      @total_price += @sum #商品合計=商品合計+小計
-    end
-
-    
-    @items = Item.all #注文した商品を全て表示したいからall?
-    @customer = Customer.find(params[:id])
+    @orders = Order.all
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    render :show
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
