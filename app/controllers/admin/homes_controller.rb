c3fd2ed4b9ef22de45d0f4830671c3fa9cda1612
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-08-24 06:37:30 +0000

    2回目テスト

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,7 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @orders = Order.page(params[:page]).per(10)
+    @orders = Order.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   private
